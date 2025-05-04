0ba25a519694024c1df556b9c6dce0fb59ceafb7
Navneet2605 <navneetpcs3771@gmail.com>
2025-05-04 23:46:27 +0530

    setinng up db

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <main>
-    <div>
-  
+    <div className="flex items-center justify-center h-screen ">
+          <div>
+                <h2 className="text-red-800"> centered content </h2>
+                <Button variant='destructive'>Click Here</Button>
+          </div>
     </div>
    </main>
   );
